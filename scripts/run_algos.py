eaf86d89d2c1cdc1dc300027701419af3773d859
Ian Henderson <ian.james.henderson@gmail.com>
2026-05-29 14:37:54 -0700

    refined plotting and cut edge methodology

--- scripts/run_algos.py
+++ scripts/run_algos.py
@@ -1,7 +1,6 @@
 """ This module runs MST and APSP algorithms on IMR and MMR graphs """
 
 import networkx as nx
-from pyvis.network import Network
 import plotly.graph_objects as go
 
 
@@ -75,28 +74,10 @@ fig.add_trace(go.Choropleth(
 def generate_mst(graph: nx.Graph) -> nx.Graph:
     """Generate MST for a NetworkX graph loaded from GraphML."""
     mst = nx.minimum_spanning_tree(graph)
-
-    """
-    # pyvis approach
-
-    mst = nx.minimum_spanning_tree(G_mmr)
-    print(sorted(mst.edges(data=True), key=lambda x:x[2].get("weight", 1)))
-
-    for u, v in G_mmr.edges():
-        G_mmr[u][v]['color'] = 'lightgray'
-        G_mmr[u][v]['width'] = 1
-
-    for u, v in mst.edges():
-        G_mmr[u][v]['color'] = 'green'
-        G_mmr[u][v]['width'] = 4
-    net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
-    net.from_nx(G_mmr)
-    net.toggle_physics(False)
-    net.show("mst_interactive.html", notebook=False)
-    """
     return mst
 
-def plot_mst(mst: nx.Graph) -> None:
+def plot_mst(mst: nx.Graph, cut_edges: list) -> None:
+    # Add MST edges to figure
     mst_lons, mst_lats = [], []
     for u, v in mst.edges():
         mst_lons.extend([STATE_CENTROIDS[u][1], STATE_CENTROIDS[v][1], None])
@@ -109,6 +90,21 @@ def plot_mst(mst: nx.Graph) -> None:
         name='MST edges',
     ))
 
+    # Overlay cut edges
+    cut_lons, cut_lats = [], []
+    for u, v, w in cut_edges:
+        cut_lons.extend([STATE_CENTROIDS[u][1], STATE_CENTROIDS[v][1], None])
+        cut_lats.extend([STATE_CENTROIDS[u][0], STATE_CENTROIDS[v][0], None])
+
+    fig.add_trace(go.Scattergeo(
+        lon=cut_lons, lat=cut_lats,
+        mode='lines',
+        line=dict(width=2.5, color='red'),
+        name='Cut edges',
+    ))
+
+
+    # Add state labels
     fig.add_trace(go.Scattergeo(
         lon=[c[1] for c in STATE_CENTROIDS.values()],
         lat=[c[0] for c in STATE_CENTROIDS.values()],
@@ -124,18 +120,55 @@ def plot_mst(mst: nx.Graph) -> None:
     fig.update_layout(title='MMR MST with cut edges', width=900, height=600)
     fig.show()
 
+def generate_cut_edges(mst: nx.Graph, weight_break: float) -> list:
+    cut_edges = []
+    mst_edges = list(mst.edges(data=True))
+    for edge in mst_edges:
+        if edge[2]["weight"] > weight_break:
+            cut_edges.append(edge)
+    return cut_edges
+
+
 def main():
     G_imr = nx.read_graphml('graphs/imr_graph.graphml')
     G_mmr = nx.read_graphml('graphs/mmr_graph.graphml')
 
+    # Generate and print IMR MST
     mst_imr = generate_mst(G_imr)
-    # if mst_imr:
-    #    print(sorted(mst_imr.edges(data=True), key=lambda x:x[2].get("weight", 1)))
+    if mst_imr:
+        print("\nIMR Edge Weights:")
+        sorted_edges = sorted(mst_imr.edges(data=True), key=lambda x:x[2].get("weight", 1))
+        for line in sorted_edges:
+            print(line)
+
+    # Generate and print MMR MST
     mst_mmr = generate_mst(G_mmr)
     if mst_mmr:
-        print(sorted(mst_mmr.edges(data=True), key=lambda x:x[2].get("weight", 1)))
+        print("\nMMR Edge Weights:")
+        sorted_edges = sorted(mst_mmr.edges(data=True), key=lambda x:x[2].get("weight", 1))
+        for line in sorted_edges:
+            print(line)
 
-    plot_mst(mst_mmr)
+    # Generate and print MMR cut edges
+    mmr_cut_edges = generate_cut_edges(mst_mmr, 4)
+    if mmr_cut_edges:
+        print("\nMMR Cut Edges")
+        for line in mmr_cut_edges:
+            print(line)
+    
+    # Generate and print IMR cut edges
+    imr_cut_edges = generate_cut_edges(mst_imr, 6)
+    if imr_cut_edges:
+        print("\nIMR Cut Edges")
+        for line in imr_cut_edges:
+            print(line)
+
+    # Plot MMR MST
+    # plot_mst(mst_mmr, mmr_cut_edges)
+
+    # Plit IMR MST
+    plot_mst(mst_imr, imr_cut_edges)
+
 
 if __name__ == '__main__':
     main()
